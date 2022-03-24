0fe8f18d7c178a61868558e20f242af6dcea508e
Daniel Alonso Pardo <danielalonsopardo@gmail.com>
2022-03-24 16:51:23 +0100

    Added login bar component

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,22 +1,13 @@
 import React, { Fragment } from 'react';
-import { LoginForm } from '/imports/ui/LoginForm.jsx';
+import { LoginBar } from '/imports/ui/LoginBar.jsx';
 import { useTracker } from 'meteor/react-meteor-data';
 import { Meteor } from 'meteor/meteor';
 
 export const App = () => {
   const user = useTracker(() => Meteor.user());
-  const logout = () => Meteor.logout();
   return (
     <div>
-      { user
-        ? ( <Fragment>
-              <div>LOGGED IN</div>
-              <button onClick={ () => logout() } >Logout</button>
-            </Fragment> )
-        : ( <Fragment>
-              <div>LOGGED OUT</div>
-              <LoginForm/> 
-            </Fragment> ) }
+      <LoginBar/>
       <h1>Play 1024!</h1>
     </div>
   )
