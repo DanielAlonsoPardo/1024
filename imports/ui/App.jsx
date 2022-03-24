b69986709a5eab716c3efab0e5d0b6f28312dece
Daniel Alonso Pardo <danielalonsopardo@gmail.com>
2022-03-24 16:13:09 +0100

    Added login bar prototype

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,7 +1,23 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import { LoginForm } from '/imports/ui/LoginForm.jsx';
+import { useTracker } from 'meteor/react-meteor-data';
+import { Meteor } from 'meteor/meteor';
 
-export const App = () => (
-  <div>
-    <h1>Play 1024!</h1>
-  </div>
-);
+export const App = () => {
+  const user = useTracker(() => Meteor.user());
+  const logout = () => Meteor.logout();
+  return (
+    <div>
+      { user
+        ? ( <Fragment>
+              <div>LOGGED IN</div>
+              <button onClick={ () => logout() } >Logout</button>
+            </Fragment> )
+        : ( <Fragment>
+              <div>LOGGED OUT</div>
+              <LoginForm/> 
+            </Fragment> ) }
+      <h1>Play 1024!</h1>
+    </div>
+  )
+};
