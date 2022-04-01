fd2504a1e2374d57576c6ef709058b24c8b6db0f
Daniel Alonso Pardo <danielalonsopardo@gmail.com>
2022-04-01 17:40:04 +0200

    Added sliding in every direction

--- imports/lib/Ten24/Ten24.js
+++ imports/lib/Ten24/Ten24.js
@@ -17,6 +17,7 @@ console.log("loading 1024...");
 
 export class Engine {
   /* Creates an empty board, ready to play */
+  /* this.board[row][col] */
   constructor(boardsize, seed) {
     if (boardsize < 1 || boardsize > 16)
       throw "Boards of size " + boardsize + "are not permitted";
@@ -28,40 +29,98 @@ export class Engine {
       this.board[i] = Array(boardsize).fill(0);
   }
 
-  slide_numbers() {
-    //slide to the right
 
-    let lower_bound = this.board.length - 1;
-    let upper_bound = 0;
+/**
+ * Slide or combine numbers on the board as far as possible in some direction.
+ *   - Zeros are empty cells, and any number can slide into it
+ *   - Two equal numbers that slide into each other combine into one by addition
+ *   - You can't chain combinations
+ */
 
+  //
+  /**
+   * slide numbers from "higher" side to "lower" side
+   *
+   * the 'start' that 'slideAwayFromStart' is referring to is top left (board[0][0])
+   * You can control which of the four sides of a board to slide towards like so:
+   *   Right: slide_numbers_raw(true, false)
+   *   Left: slide_numbers_raw(false, false)
+   *   Up: slide_numbers_raw(false, true)
+   *   Down: slide_numbers_raw(true, true)
+   */
+//  slide_numbers_raw(high_position, low_position, vertical) {
+  slide_numbers_raw(slideAwayFromStart, slideVertically) {
+    let lower_side;
+    let higher_side;
+    //these functions are essentially used to iterate through board cells
+    //they change depending on what side you're sliding towards
+    let next_position;
+    let cell_within_bounds;
+    let set_cell
+    let get_cell
+
+    //sliding towards or away from start (0,0)?
+    if (slideAwayFromStart) {//either right or down
+      lower_side = this.board.length - 1;
+      higher_side = 0;
+      next_position = c => c-1;
+      cell_within_bounds = c => (c >= higher_side)
+    } else {//either up or left
+      lower_side = 0;
+      higher_side = this.board.length - 1;
+      next_position = c => c+1;
+      cell_within_bounds = c => (c <= higher_side);
+    }
+
+    //sliding vertically or horizontally?
+    if (slideVertically) {//either up or down
+      //go column by column
+      set_cell = (col, cell, value) => this.board[cell][col] = value
+      get_cell = (col, cell) => this.board[cell][col]
+    } else {//either left or right
+      //go row by row
+      set_cell = (row, cell, value) => this.board[row][cell] = value
+      get_cell = (row, cell) => this.board[row][cell]
+    }
+
+    //for each row (or column) of cells
     for (let row = 0; row < this.board.length; row++) {
-      let from = lower_bound - 1;
-      let to = lower_bound;
+      let from = next_position(lower_side);
+      let to = lower_side;
 
-
-      while(from >= 0) {
-        if (this.board[to][row] == 0) {
-          if (this.board[from][row] != 0) {
-            this.board[to][row] = this.board[from][to];
-            this.board[from][row] = 0;
+      //slide them to one side
+      while(cell_within_bounds(from)) {
+        if (get_cell(row, to) == 0) {
+          //'to' cell is empty -> slide 'from' into it
+          //...if 'from' has something
+          if (get_cell(row, from) != 0) {
+            set_cell(row, to, get_cell(row, from));
+            set_cell(row, from, 0);
           }
         } else {
-          if (this.board[from][row] == this.board[to][row]) {
-            this.board[from][row] = 0;
-            this.board[to][row] *= 2;
-            to--;
-          }
+          if (get_cell(row, to) == get_cell(row, from)) {
+            //'to' can be combined with 'from'
+            set_cell(row, to, get_cell(row, from) + get_cell(row, to));
+            set_cell(row, from, 0);
+            to = next_position(to);
+          } else if (get_cell(row, from) != 0) {
+            //'to' can't be combined with 'from'
+            to = next_position(to);
+            if (from != to) {
+              //there's room to slide 'to' towards 'from'
+              set_cell(row, to, this.board[row][from]);
+              set_cell(row, from, 0);
+            }
+          }//else 'to' is zero and can be skipped
         }
-        from--;
+        from = next_position(from);
       }
-
-
     }
-  };
-}
+  }
 
-export const Ten24 = () => {
-
-
-  return 
+  log_board = () => {
+    for (const l of engine.board)
+      console.log(l)
+    console.log()
+  }
 }
--- imports/lib/Ten24/Ten24.test.js
+++ imports/lib/Ten24/Ten24.test.js
@@ -34,18 +34,67 @@ export const UnitTests = () => {
           engine = new Ten24.Engine(boardsize, 0);
         });
 
+
+
         it("slides to the right", function() {
           engine = new Ten24.Engine(4, 0);
-          engine.board[1][1] = 1;
-          engine.slide_numbers();
-          /* [ ][ ][ ][ ] ->  [ ][ ][ ][ ]
-           * [ ][1][ ][ ] ->  [ ][>][ ][1]
-           * [ ][ ][ ][ ] ->  [ ][ ][ ][ ]
-           * [ ][ ][ ][ ] ->  [ ][ ][ ][ ]
-           */
+          engine.board[0] = [0, 0, 1, 1];
+          engine.board[1] = [0, 1, 0, 0];
+          engine.board[2] = [1, 1, 1, 1];
+          engine.board[3] = [0, 1, 2, 1];
+
+          engine.slide_numbers_raw(true, false);
 
           assert.equal(engine.board[1][1], 0, "number stayed in place");
+          assert.equal(engine.board[1][3], 1, "number did not slide right");
 
+
+          assert.deepEqual(engine.board[0], [0, 0, 0, 2], "numbers did not slide to the right");
+          assert.deepEqual(engine.board[1], [0, 0, 0, 1], "numbers did not slide to the right");
+          assert.deepEqual(engine.board[2], [0, 0, 2, 2], "numbers did not slide to the right");
+          assert.deepEqual(engine.board[3], [0, 1, 2, 1], "numbers did not slide to the right");
+        });
+        it("slides to the left", function() {
+          engine = new Ten24.Engine(4, 0);
+          engine.board[0] = [0, 0, 1, 1];
+          engine.board[1] = [0, 1, 0, 0];
+          engine.board[2] = [1, 1, 1, 1];
+          engine.board[3] = [0, 1, 2, 1];
+
+          engine.slide_numbers_raw(false, false);
+
+          assert.deepEqual(engine.board[0], [2, 0, 0, 0], "numbers did not slide to the left (row 0)");
+          assert.deepEqual(engine.board[1], [1, 0, 0, 0], "numbers did not slide to the left (row 1)");
+          assert.deepEqual(engine.board[2], [2, 2, 0, 0], "numbers did not slide to the left (row 2)");
+          assert.deepEqual(engine.board[3], [1, 2, 1, 0], "numbers did not slide to the left (row 3)");
+        });
+        it("slides down", function() {
+          engine = new Ten24.Engine(4, 0);
+          engine.board[0] = [0, 0, 1, 1];
+          engine.board[1] = [0, 1, 0, 0];
+          engine.board[2] = [1, 1, 1, 1];
+          engine.board[3] = [0, 1, 2, 1];
+
+          engine.slide_numbers_raw(true, true);
+
+          assert.deepEqual(engine.board[0], [0, 0, 0, 0], "numbers did not slide down (row 0)");
+          assert.deepEqual(engine.board[1], [0, 0, 0, 0], "numbers did not slide down (row 1)");
+          assert.deepEqual(engine.board[2], [0, 1, 2, 1], "numbers did not slide down (row 2)");
+          assert.deepEqual(engine.board[3], [1, 2, 2, 2], "numbers did not slide down (row 3)");
+        });
+        it("slides up", function() {
+          engine = new Ten24.Engine(4, 0);
+          engine.board[0] = [0, 0, 1, 1];
+          engine.board[1] = [0, 1, 0, 0];
+          engine.board[2] = [1, 1, 1, 1];
+          engine.board[3] = [0, 1, 2, 1];
+
+          engine.slide_numbers_raw(false, true);
+
+          assert.deepEqual(engine.board[0], [1, 2, 2, 2], "numbers did not slide down (row 0)");
+          assert.deepEqual(engine.board[1], [0, 1, 2, 1], "numbers did not slide down (row 1)");
+          assert.deepEqual(engine.board[2], [0, 0, 0, 0], "numbers did not slide down (row 2)");
+          assert.deepEqual(engine.board[3], [0, 0, 0, 0], "numbers did not slide down (row 3)");
         });
       });
     });
