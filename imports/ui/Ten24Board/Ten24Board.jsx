19e6a339b4a25cdbce169967caebb6f749696322
Daniel Alonso Pardo <danielalonsopardo@gmail.com>
2022-04-11 16:20:17 +0200

    Fixed issue with cell ID not updating correctly

--- imports/ui/Ten24Board/Ten24Board.jsx
+++ imports/ui/Ten24Board/Ten24Board.jsx
@@ -53,12 +53,15 @@ let renderNumber = (number) => {
   + `ten24-row-${number.position.row} `
   + `ten24-col-${number.position.column} `
   + (number.slide ? `slide-from-${number.slide.direction}-${number.slide.distance} sliding` : "");
-  return (<div className={ classNames } key={ number.id }> </div>);
+  return (<div className={ classNames } key={ number.id }> { number.value } </div>);
 }
 
 export const Ten24Board = () => {
   const [numbersInPlay, setNumbersInPlay] = useState([]);
+  const [tempNumbers, setTempNumbers] = useState([]);
   const [nID, setID] = useState(0);
+  let ID = nID;
+  const [counter, setCounter] = useState(0);
 
   let placeNumberFake = () => {
     placeNumber(0, 0, 2);
@@ -67,58 +70,79 @@ export const Ten24Board = () => {
     placeNumber(3, 3, 2);
   }
 
+  let placeNumberFake2 = () => {
+    placeNumber(3, 0, 2);
+    placeNumber(3, 0, 2);
+    placeNumber(3, 0, 2);
+  }
+  let combineNumbersFake = () => {
+    combineNumbers({row: 3, column: 0}, 33);
+  }
+
   let slideNumberFake = () => {
     let pos = (row, col) => ({ row: row, column: col })
     let to = (dist, dir) => ({ distance: dist, direction: dir })
-    slideNumber(pos(0, 0), to(3, "right"));
-    slideNumber(pos(1, 1), to(2, "right"));
-    slideNumber(pos(2, 2), to(1, "right"));
+    slideNumber(pos(0, 0), pos(0, 3), to(3, "right"));
+    slideNumber(pos(1, 1), pos(1, 3), to(2, "right"));
+    slideNumber(pos(2, 2), pos(2, 3), to(1, "right"));
   }
 
-  let placeNumber = (row, column, value) => {
+  let placeNumber = (row, column, value, combined) => {
     let number = {
       position: {
         row,
         column
       },
-      combined: 0,
+      combined: combined || 0,
       slide: null,
       value,
-      id: nID
+      id: ID
     }
 
 //    setNumbersInPlay([...numbersInPlay, number]);
     numbersInPlay.push(number);//needs a re-render
-    setID(nID + 1);
+    setID(++ID);
   }
 
   /* from = { row, column }
-     to = { distance, direction }
+     to = { row, column }
+     travel = { distance, direction } // what movement the number did to go from `from` to `to`
    */
-  let slideNumber = (from, to) => {
+  let slideNumber = (from, to, travel) => {
     let numberFrom = (n) => (n.position.column == from.column &&
                              n.position.row == from.row &&
                              n.slide == null)
     let n = numbersInPlay.find(numberFrom);
-
     if (!n) return;
-    n.slide = { ...to };
+
+    n.position = to;
+    n.slide = { ...travel };
 
     //setNumbersInPlay([...numbersInPlay]);//if this line is commented you need to force re-render
-    setID(nID + 1);//only used to force a re-render.
+    setID(++ID);//only used to force a re-render.
   }
 
-  let combineNumbers = () => {
+  // pos = { row, column }
+  let combineNumbers = (pos, value) => {
+    let overwritable = (number) => (number.position.row == pos.row &&
+                                    number.position.column == pos.column)
+    //move old to temp numbers array
+    setNumbersInPlay(numbersInPlay.filter(n => !overwritable(n)));
+    let to_combine = numbersInPlay.filter(overwritable);
+    setTempNumbers(tempNumbers.concat(to_combine));
+
+    //add new
+    let combined = Math.max(...(to_combine.map(n => n.slide?.distance)));
+    placeNumber(pos.row, pos.column, value, combined || 0);
   }
-  let lilhelper = { column: 1, row: 1}
   return (
+<div>
     <div className="ten24-board">
       <button onClick={ e => { placeNumberFake() } }>placeNumberFake</button>
       <button onClick={ e => { slideNumberFake() } }>slideNumberFake</button>
-      <button onClick={ e => { slideNumber(lilhelper, { direction: "up", distance: 1 }) } }>up</button>
-      <button onClick={ e => { slideNumber(lilhelper, { direction: "down", distance: 1 }) } }>down</button>
-      <button onClick={ e => { slideNumber(lilhelper, { direction: "left", distance: 1 }) } }>left</button>
-      <button onClick={ e => { slideNumber(lilhelper, { direction: "right", distance: 1 }) } }>right</button>
+      <button onClick={ e => { placeNumberFake2() } }>place combine</button>
+      <button onClick={ e => { combineNumbersFake() } }>test combine</button>
+
       <div className="ten24-board-background-layer">
       </div>
       <div className="ten24-board-empty-cell-layer ten24-board-layer">
@@ -126,7 +150,11 @@ export const Ten24Board = () => {
       </div>
       <div className="ten24-board-numbers-layer ten24-board-layer">
         { numbersInPlay.map(renderNumber) }
+        { tempNumbers.map(renderNumber) }
       </div>
     </div>
+<p>{JSON.stringify(numbersInPlay)}</p>
+<p>{JSON.stringify(tempNumbers)}</p>
+</div>
   )
 }
