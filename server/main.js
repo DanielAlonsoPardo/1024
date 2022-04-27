a6da7c4e8dcccf98aa29ea8329131ee4548fcecb
Daniel Alonso Pardo <danielalonsopardo@gmail.com>
2022-04-27 15:39:58 +0200

    Implemented verification hash generation for new users

--- server/main.js
+++ server/main.js
@@ -5,45 +5,52 @@ import { Roles } from 'meteor/alanning:roles';
 
 import Leaderboard from '/imports/api/Leaderboard/Leaderboard.js';
 import Seeding from './seeding.js';
+import crypto from 'crypto';
 
+/* Startup */
 Meteor.startup(() => {
   if (!process.env.MAIL_URL) {
     console.warn("environment variable for mailing not set!");
   }
 
-  Roles.createRole("verified", {unlessExists: true});
   Seeding.Startup();
+
+  /* Publications */
+  Leaderboard.Publish();
+
+  //current user's roles
+  //https://github.com/Meteor-Community-Packages/meteor-roles#installing
+  Meteor.publish(null, function () {
+    if (this.userId) {
+      return Meteor.roleAssignment.find({ 'user._id': this.userId });
+    } else {
+      this.ready();
+    }
+  });
 });
 
+/* Roles */
+Roles.createRole("verified", { unlessExists: true });
+
+/* Server methods */
 Meteor.methods({
   sendVerificationEmail() {
+    let hash = getRandomVerificationHash();
     let email = {
       to: "danielalonsopardo@gmail.com",
       from: "danielalonsopardo@gmail.com",
       subject: "Verification email - 1024",
-      text: "Hello, World!"
+      text: "Verification hash: " + hash,
     }
     console.log("Pretending to send an email...");
+    console.log("Generated verification hash " + hash);
 //    Email.send(email);
     return true;
-  }
+  },
 });
 
-//publications
-Leaderboard.Publish();
-
-//user roles
-//https://github.com/Meteor-Community-Packages/meteor-roles#installing
-Meteor.publish(null, function () {
-  if (this.userId) {
-    return Meteor.roleAssignment.find({ 'user._id': this.userId });
-  } else {
-    this.ready()
-  }
-});
-
+/* Accounts */
 Accounts.validateLoginAttempt((attempt) => {
-  console.log("LOGIN ATTEMPT");
   if (!attempt.allowed) {
     return false;
   }
@@ -51,3 +58,13 @@ Accounts.validateLoginAttempt((attempt) => {
   console.log("success");
   return Roles.userIsInRole(attempt.user._id, "verified");
 })
+
+function getRandomVerificationHash() {
+  let hash = crypto.createHash('sha256');
+  return hash.update(crypto.randomBytes(10)).digest('hex');
+}
+
+Accounts.onCreateUser((options, user) => {
+  user.verification = getRandomVerificationHash();
+  return user;
+});
